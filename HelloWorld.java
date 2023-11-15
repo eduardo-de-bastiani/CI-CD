521a7b459b4f99e236f9541ecc787bddc8ab7ecc
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2023-11-15 11:19:35 -0300

    teste 2 do github actions

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
         System.out.println("Hello World!");
 
         //testando o github actions com alteracao no arquivo HelloWorld.java
+        //teste 2 do github actions
     }
 }
